4f7af44367d88de93d22c3ede2d956e090b69591
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-05-12 17:59:48 +0200

    feat: update keymap

--- hosts/darwin/main/files/nvim/lua/ryu/core/keymaps.lua
+++ hosts/darwin/main/files/nvim/lua/ryu/core/keymaps.lua
@@ -2,3 +2,6 @@ vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
